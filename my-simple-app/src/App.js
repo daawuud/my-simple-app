37163ba9ab35ab64938db6b410b71727818066c2
Daud Mohamed <daudmohamed@Dauds-MacBook-Pro.local>
2024-10-03 17:41:57 -0600

    Init Commit

---
+++ my-simple-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Simple React App</h1>
+        <p>This is a basic example of a React application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
